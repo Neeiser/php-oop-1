8ac6e4a0842b6462a3803bcdd5d74602355ed8a1
Neeiser <stortini.study@gmail.com>
2022-04-29 15:55:17 +0200

    some class structure - Variables + costructor

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php
 
+/*
+1: Create un file index.php in cui.
+2: Definire una classe ‘Movie’
+    => all'interno della classe sono dichiarate delle variabili d'istanza
+    => all'interno della classe è definito un costruttore
+    => all'interno della classe è definito almeno un metodo
+3:vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà  
+*/
 class Movie
 {
-    private $moviename;
+    /* variabili d'istanza */
+    private $name;
+    private $director;
+    private $length;
+    private $genre;
+
+    public function __construct($_name, $_length, $_genre)
+    {
+        $this->$_name;
+        /* $this->$_director; -------> Opzionale, non lo metto, mi va bene null come default*/
+        $this->$_length;
+        $this->$_genre;
+    }
 }
 
 ?>
